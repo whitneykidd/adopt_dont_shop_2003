28b27d38ded0062c371df596437a53d60e05d5ab
Whitney Kidd <whitneymkidd@gmail.com>
2020-05-05 16:05:21 -0600

    create user_can_see_one_shelter_spec

---
+++ spec/features/shelters/user_can_see_one_shelter_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "user sees one shelter" do
+  it "can see one shelter with all its attributes" do
+    shelter = Shelter.create(name: "Find-a-Friend",
+                              address: "123 North Street",
+                              city: "Denver",
+                              state: "CO",
+                              zip: 80223 )
+
+    visit shelter_path
+
+    click_link shelter.name
+
+    expect(page).to have_content(shelter.name)
+    expect(page).to have_content(shelter.address)
+    expect(page).to have_content(shelter.city)
+    expect(page).to have_content(shelter.state)
+    expect(page).to have_content(shelter.zip)
+  end
+end
