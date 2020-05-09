f9dd56eb36cb931a94efd4da21923c3176f64937
Whitney Kidd <whitneymkidd@gmail.com>
2020-05-09 15:31:11 -0600

    create pet#delete

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -30,6 +30,11 @@ class PetsController < ApplicationController
     redirect_to "/pets/#{@pet.id}"
   end
 
+  def destroy
+    Pet.destroy(params[:id])
+    redirect_to "/pets"
+  end
+
   private
 
   def pet_params
